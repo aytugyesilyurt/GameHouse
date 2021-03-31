518f4cbc3776c9da553ec066cae07d2705158e2f
Aytuğ YEŞİLYURT <agyesilyurt@hotmail.com>
2021-03-31 17:03:05 +0300

    Add project files.

---
+++ GameHouse/CustomerManagerService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameHouse
+{
+    class CustomerManagerService
+    {
+        public void SignIn(CustomerManager customer)
+        {
+
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " isimli üyelik oluşturuldu.");
+        }
+
+        public void Update(CustomerManager customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " isimli üyelik başarıyla güncellendi.");
+        }
+
+        public void Delete(CustomerManager customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName + " isimli üyelik sistemden silindi.");
+        }
+    }
+}
---
+++ GameHouse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
